c9f21140f202ed37c5561b5624e5a31be4e14d82
Tribulacion <158357022+Tribulacion@users.noreply.github.com>
2024-11-28 00:05:52 +0000

    Agregados setters y Getters en la clase Cliente
    archivo modificado cliente.py

--- cliente.py
+++ cliente.py
@@ -1,9 +1,41 @@
 class Cliente:
     def __init__(self, id=None, nombre=None, apellido=None, membresia=None):
-        self.id = id
-        self.nombre = nombre
-        self.apellido = apellido
-        self.membresia = membresia
+        self._id = id
+        self._nombre = nombre
+        self._apellido = apellido
+        self._membresia = membresia
+    
+    @property
+    def id(self):
+        return self._id
+    
+    @id.setter
+    def id(self, id):
+        self._id = id
+    
+    @property
+    def nombre(self):
+        return self._nombre
+    
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
+    
+    @property
+    def apellido(self):
+        return self._apellido
+    
+    @apellido.setter
+    def apellido(self, apellido):
+        self._apellido = apellido
+    
+    @property
+    def membresia(self):
+        return self._membresia
+    
+    @membresia.setter
+    def membresia(self, membresia):
+        self._membresia = membresia
 
     def __str__(self):
         return (f'Id: {self.id}, Nombre: {self.nombre}, '
